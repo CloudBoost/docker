54e2443731ed50c56227346d9206189b3e543ad3
Nawaz Dhandala <nawazdhandala@outlook.com>
2016-08-09 18:20:31 +0530

    Ubuntu script added

---
+++ ubuntu16/install.sh
@@ -0,0 +1,29 @@
+#Install Docker
+uname -r
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo touch /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee  /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get --assume-yes purge lxc-docker
+sudo apt-cache policy docker-engine
+sudo apt-get update
+sudo apt-get --assume-yes install linux-image-extra-$(uname -r)
+sudo apt-get --assume-yes install docker-engine
+sudo service docker start
+
+#Install Docker Compose. 
+sudo apt-get -y install python-pip
+sudo pip install docker-compose
+
+#Clone CloudBoost docker repo. 
+sudo apt-get -y install git
+git clone https://github.com/CloudBoost/docker.git
+cd docker
+
+#Run Docker compose. 
+sudo docker-compose up
+
+
+
---
+++ ubuntu16/startup.sh
@@ -0,0 +1,10 @@
+#Install Git
+sudo apt-get update
+sudo apt-get -y install git
+
+#Clone Repo
+git clone https://github.com/CloudBoost/docker.git
+cd docker
+
+#Run the script. 
+sudo bash ubuntu_16_install.sh
