3c369d0db15ff5fcc9d24699e52840bd907e050e
Nawaz Dhandala <hello@nawazdhandala.com>
2016-08-24 11:40:34 +0530

    Update install.sh

--- ubuntu16/install.sh
+++ ubuntu16/install.sh
@@ -1,26 +1,31 @@
 #!/bin/sh -
 
 #Install Docker
-uname -r
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo touch /etc/apt/sources.list.d/docker.list
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee  /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get --assume-yes purge lxc-docker
-sudo apt-cache policy docker-engine
-sudo apt-get update
-sudo apt-get --assume-yes install linux-image-extra-$(uname -r)
-sudo apt-get --assume-yes install docker-engine
+if which docker </dev/null; then
+  uname -r
+  sudo apt-get update
+  sudo apt-get install apt-transport-https ca-certificates
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  sudo touch /etc/apt/sources.list.d/docker.list
+  sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee  /etc/apt/sources.list.d/docker.list
+  sudo apt-get update
+  sudo apt-get --assume-yes purge lxc-docker
+  sudo apt-cache policy docker-engine
+  sudo apt-get update
+  sudo apt-get --assume-yes install linux-image-extra-$(uname -r)
+  sudo apt-get --assume-yes install docker-engine
+fi
 sudo service docker start
 
 #Install Docker Compose. 
-sudo apt-get -y install python-pip
-sudo pip install docker-compose
-
+if which docker-compose </dev/null; then
+  sudo apt-get -y install python-pip
+  sudo pip install docker-compose
+fi
 #Clone CloudBoost docker repo. 
-sudo apt-get -y install git
+if which git </dev/null; then
+  sudo apt-get -y install git
+fi
 git clone https://github.com/CloudBoost/docker.git
 cd docker
 
